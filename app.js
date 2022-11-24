13f0346b72ecb1b1710509908bbd6155511f6180
Any-Winter-4079 <50542132+Any-Winter-4079@users.noreply.github.com>
2022-11-24 15:52:00 +0100

    remove debug test file name change and logs

--- app.js
+++ app.js
@@ -42,10 +42,6 @@ app.use(passport.session());
 express_logger.initializeLogger(app);
 const logger = express_logger.getLogger();
 
-console.log(maxPwdLength);
-console.log(mongoDBURI);
-console.log(dbModelName);
-
 // 5. set up and connect to database
 mongoose.set("sanitizeFilter", true);
 const Schema = mongoose.Schema;
