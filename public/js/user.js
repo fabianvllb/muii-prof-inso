72b371ea87f279b4de9575a87e55e750af7a25f1
Fabian Villalobos <fabian.villalobos@alumnos.upm.es>
2023-02-12 14:27:45 +0100

    added subscription feed

--- public/js/user.js
+++ public/js/user.js
@@ -1,22 +1,63 @@
-if (document.readyState == "loading") {
-    document.addEventListener("DOMContentLoaded", userReady);
-} else {
-    userReady();
+const subInput = document.getElementById("url-input");
+const subButton = document.getElementById("add-new-site-button");
+const subList = document.querySelector(".subscription-list");
+
+subButton.addEventListener("click", addSite);
+
+function addSite(event){
+  event.preventDefault();
+
+  const subscriptionCard = document.createElement("div");
+  subscriptionCard.className = "feed-card";
+
+  const cardHeader = document.createElement("div");
+  cardHeader.className = "card-header";
+  subscriptionCard.appendChild(cardHeader);
+
+  const headerTitle = document.createElement("p");
+  headerTitle.className = "card-title";
+  headerTitle.innerText = "Title";
+  cardHeader.appendChild(headerTitle);
+
+  const cardTitleSeparator = document.createElement("p");
+  cardTitleSeparator.className = "card-title-separator";
+  cardTitleSeparator.innerText = "-";
+  cardHeader.appendChild(cardTitleSeparator);
+
+  const cardURL = document.createElement("p");
+  cardURL.innerText = subInput.value;
+  cardHeader.appendChild(cardURL);
+
+  const cardDescription = document.createElement("p");
+  cardDescription.className = "card-description";
+  cardDescription.innerText = "Description: No description yet";
+  subscriptionCard.appendChild(cardDescription);
+
+  const cardButtons = document.createElement("div");
+  cardButtons.className = "card-buttons";
+  subscriptionCard.appendChild(cardButtons);
+
+  const siteButton = document.createElement("a");
+  siteButton.innerText = "Site";
+  cardButtons.appendChild(siteButton);
+
+  const editButton = document.createElement("a");
+  editButton.innerText = "Edit";
+  cardButtons.appendChild(editButton);
+
+  subList.appendChild(subscriptionCard);
+  subInput.value = "";
 }
 
-function userReady() {
-    document.getElementById("button").addEventListener("click", logoutEvent);
- }
-
- function logoutEvent(event) {
-    event.preventDefault();
-    axios({
-        method: "post",
-        url: "/logout",
-        data: {},
-        headers: { "Content-Type": "application/json" },
-      }).then(function(response) {
-        window.location.href = "/";
-      }).catch (function(error) {
-      });
- }
+function logoutEvent(event) {
+  event.preventDefault();
+  axios({
+      method: "post",
+      url: "/logout",
+      data: {},
+      headers: { "Content-Type": "application/json" },
+    }).then(function(response) {
+      window.location.href = "/";
+    }).catch (function(error) {
+    });
+}
